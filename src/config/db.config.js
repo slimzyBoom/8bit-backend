4fa27c16eeedc83fd2159b2945f1fbdc28bf7191
That_Tech_Guy <169068459+slimzyBoom@users.noreply.github.com>
2025-04-25 14:29:43 -1200

    feat: setting-up codebase

---
+++ src/config/db.config.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+const MONGO_URI = process.env.MONGO_URI
+
+export const connectDB = async () => {
+    try {
+        await mongoose.connect(MONGO_URI)
+        console.log("Database connected successfully")
+    } catch (error) {
+        console.log(`Error connecting to DB: ${error.message}`)
+        process.exit(1);
+    }
+}
