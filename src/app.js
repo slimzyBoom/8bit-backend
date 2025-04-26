7da7f0ba5e7c7cb5a4d145e53f7908f4f0f5b780
That_Tech_Guy <169068459+slimzyBoom@users.noreply.github.com>
2025-04-25 14:31:58 -1200

    setting up codebase

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
+import connectDB from "./config/db.config"
 
 const app = express();
+app.use(connectDB); // Connect to the database
 
 app.use(cors());
 app.use(morgan('dev'));
